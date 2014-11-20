d96950aedb93c502f0d4e76ec8c8d1519f52e099
Pramodh Yapa <pramodh@boreas.triumf.ca>
2014-11-20 06:51:47 -0800

    Corrected transmission functions

--- neutron.h
+++ neutron.h
@@ -136,21 +136,16 @@ protected:
 				switch (Interaction) { //This switch changes to the appropriate model depending on what the int Interaction is
  					case 0: { //Specular case
 						//************** Snell's Law Transmission **************
-						long double k1 = sqrt(Enormal); // wavenumber in first solid (use only real part for transmission!)
-						long double k2 = sqrt(Enormal - Estep); // wavenumber in second solid (use only real part for transmission!)
-						DoSnellTransmission(y1, y2, normal, k1, k2);
+						DoSnellTransmission(y1, y2, normal, Estep);
 					}
 					break;
   					case 1: { //Specular-diffuse case
 						if (prob >= entering->mat.DiffProb){
-							//************** Snell's Law Transmission **************
-							long double k1 = sqrt(Enormal); // wavenumber in first solid (use only real part for transmission!)
-							long double k2 = sqrt(Enormal - Estep); // wavenumber in second solid (use only real part for transmission!)
-							DoSnellTransmission(y1, y2, normal, k1, k2);
+							DoSnellTransmission(y1, y2, normal, Estep);
 						}
 						else{
 							//************** Diffuse transmission ************
-							DoDiffuseTransmission(x1, y1, x2, y2, normal);
+							DoDiffuseTransmission(x1, y1, x2, y2, normal, Estep);
 						}
 					}
 					break;
@@ -176,22 +171,18 @@ protected:
 
 							else {
 								//************** Snell's Law Transmission **************
-								long double k1 = sqrt(Enormal); // wavenumber in first solid (use only real part for transmission!)
-								long double k2 = sqrt(Enormal - Estep); // wavenumber in second solid (use only real part for transmission!)
-								DoSnellTransmission(y1, y2, normal, k1, k2);
+								DoSnellTransmission(y1, y2, normal, Estep);
 							}
 
 						}
 						else { //If the MR transmission condition is not valid, switch to Diffuse-specular
 							if (prob >= entering->mat.DiffProb){
 								//************** Snell's Law Transmission **************
-								long double k1 = sqrt(Enormal); // wavenumber in first solid (use only real part for transmission!)
-								long double k2 = sqrt(Enormal - Estep); // wavenumber in second solid (use only real part for transmission!)
-								DoSnellTransmission(y1, y2, normal, k1, k2);
+								DoSnellTransmission(y1, y2, normal, Estep);
 							}
 							else{
 								//************** Diffuse transmission ************
-								DoDiffuseTransmission(x1, y1, x2, y2, normal);
+								DoDiffuseTransmission(x1, y1, x2, y2, normal, Estep);
 							}
 						}
 					}
@@ -440,8 +431,11 @@ protected:
 
 	}
 
-	void DoSnellTransmission(long double y1[6], long double y2[6], const long double normal[3], long double k1, long double k2) {
+	void DoSnellTransmission(long double y1[6], long double y2[6], const long double normal[3], long double Estep) {
 		long double vnormal = y1[3]*normal[0] + y1[4]*normal[1] + y1[5]*normal[2];
+		long double Enormal = 0.5*m_n*vnormal*vnormal; // energy normal to reflection plane
+		long double k1 = sqrt(Enormal); // wavenumber in first solid 
+		long double k2 = sqrt(Enormal - Estep); // wavenumber in second solid
 		for (int i = 0; i < 3; i++)
 			y2[i + 3] += (k2/k1 - 1)*(normal[i]*vnormal); // refract (scale normal velocity by k2/k1)
 	}
@@ -481,9 +475,10 @@ protected:
 
 	}
 
-	void DoDiffuseTransmission(long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3]) {
+	void DoDiffuseTransmission(long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3], long double Estep) {
 		//THIS FUNCTION NEEDS TO BE TESTED
 		long double vnormal = y1[3]*normal[0] + y1[4]*normal[1] + y1[5]*normal[2];
+		long double Enormal = 0.5*m_n*vnormal*vnormal; // energy normal to reflection plane
 		long double vabs = sqrt(y1[3]*y1[3] + y1[4]*y1[4] + y1[5]*y1[5]);
 		long double phi_r = mc->UniformDist(0, 2*pi); // generate random reflection angles (Lambert's law)
 		long double theta_r = mc->SinCosDist(0, 0.5*pi);
@@ -499,7 +494,7 @@ protected:
 //		printf("Diffuse transmission! Erefl=%LG neV w_e=%LG w_s=%LG\n",Enormal*1e9,phi_r/conv,theta_r/conv);
 	}
 
-	void DoMicroRoughnessReflection(long double FermiDiff, long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3], solid *entering, MRHelperPointerless MRFunctions) {
+	void DoMicroRoughnessReflection(long double Estep, long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3], solid *entering, MRHelperPointerless MRFunctions) {
 		bool found = false;
 		long double E = Ekin(&y1[3]); //Energy of Neutron
 		long double theta_i = IncidentTheta(y1, normal);
@@ -511,7 +506,7 @@ protected:
 			long double prob_test = mc->UniformDist(0,0.5); //this is the probability value for the given phi and theta value above,
 								     //which will be compared to see if it is above or below the actual value computed (0.5 used as max isn't available)
 			double AngCut = entering->mat.AngCut;			
-			long double prob_actual = MRFunctions.ProbIplus(E, FermiDiff, theta_i, theta_o_test, phi_o_test, b, w, AngCut);
+			long double prob_actual = MRFunctions.ProbIplus(E, Estep, theta_i, theta_o_test, phi_o_test, b, w, AngCut);
 			if (prob_test <= prob_actual) {
 				x2 = x1;
 				for (int i = 0; i < 6; i++)
@@ -527,12 +522,14 @@ protected:
 
 	}
 
-	void DoMicroRoughnessTransmission(long double FermiDiff, long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3], solid *entering, MRHelperPointerless MRFunctions) {
+	void DoMicroRoughnessTransmission(long double Estep, long double x1, long double y1[6], long double &x2, long double y2[6], const long double normal[3], solid *entering, MRHelperPointerless MRFunctions) {
 		bool found = false;
 		long double E = Ekin(&y1[3]); //Kinetic Energy of Neutron
 		long double theta_i = IncidentTheta(y1,normal);
 		long double b = entering->mat.RMSRough*1e-9; // RMS roughness of entering material in [m]
 		long double w = entering->mat.CorrelLength*1e-9; // correlation length of entering material in [m]
+		long double vnormal = y1[3]*normal[0] + y1[4]*normal[1] + y1[5]*normal[2];	
+		long double Enormal = 0.5*m_n*vnormal*vnormal; // energy normal to reflection plane	
 		while (found == false) {
 			//long double vabs = sqrt(y1[3]*y1[3] + y1[4]*y1[4] + y1[5]*y1[5]);
 			long double phi_o_test = mc->UniformDist(-1*pi,pi); //this is the potential outgoing phi value for the transmitted neutron
@@ -540,7 +537,7 @@ protected:
 			long double prob_test = mc->UniformDist(0,0.5); //this is the probability value for the given phi and theta value
 									 //above which will be compared to see if it is above or below the actual value computed
 			double AngCut = entering->mat.AngCut;
-			long double prob_actual = MRFunctions.ProbIminus(E, FermiDiff, theta_i, theta_o_test, phi_o_test, b, w, AngCut);
+			long double prob_actual = MRFunctions.ProbIminus(E, Estep, theta_i, theta_o_test, phi_o_test, b, w, AngCut);
 			if (prob_test <= prob_actual) {
 				x2 = x1;
 				for (int i = 0; i < 3; i++)
